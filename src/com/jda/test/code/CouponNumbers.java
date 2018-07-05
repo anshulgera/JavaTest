adfa1e5b3a11fcb306d7f6326b1540cea081c644
anshul gera <anshulgera@gmail.com>
2018-07-05 12:11:23 +0530

    third

--- src/com/jda/test/code/CouponNumbers.java
+++ src/com/jda/test/code/CouponNumbers.java
@@ -1,21 +1,32 @@
 package com.jda.test.code;
-import java.util.Arrays;
-
 import com.jda.test.logic.*;
-public class CouponNumbers {
 
+public class CouponNumbers {
+	
+	public static int getRandomNumber(int n){
+		return (int)(Math.random()*n);
+	}
+	
+	public static int getRandomNoCount(int n){
+		int distinct = 0;
+		int count = 0;
+		boolean[] number = new boolean[n];
+		while(distinct<n){
+			int value = getRandomNumber(n);
+			if(!number[value]){
+				number[value] = true;
+				distinct++;
+			}
+			count++;
+	}
+		return count;
+	}
+	
 	public static void main(String[] args) {
 		Utility ut = new Utility();
 		int n = ut.takeInputInteger();
-		int[] arr = new int[n];
-		for(int i=0;i<n;i++){
-			arr[i] = ut.takeInputInteger();
-		}
-		boolean[] isCollected = new boolean[n];
-		Arrays.fill(isCollected, false);
-		
-		
-
+		int count  = getRandomNoCount(n);
+		System.out.println("Random numbers needed : " + count );
 	}
 
 }
